568c2a0e880f9f940a9f77e27af8e2e26eaa5687
Peter Lundberg <peter.m.lundberg@gmail.com>
2014-12-23 03:17:40 +0100

    Reversed last commit. Created by cloudpebble bug.

---
+++ src/linkedlist.c
@@ -0,0 +1,61 @@
+#include <pebble.h>
+#include "linkedlist.h"
+
+
+void test()
+{	
+	/* This will be the unchanging first node */
+    struct node *root;      
+
+    /* Now root points to a node struct */
+    root = (struct node *) malloc( sizeof(struct node) ); 
+    root = malloc( sizeof(struct node) );  
+	
+	/* The node root points to has its next pointer equal to a null pointer set */
+    root->next = 0; 
+    /* By using the -> operator, you can modify what the node, a pointer, (root in this case) points to. */;
+    root->x = 5;
+	
+}
+
+
+/*
+void traverseLinkedList(struct node *node) {
+	if(node == NULL)
+		return;
+	node->x = 0;
+	traverseLinkedList(node->next);
+}
+*/
+
+void release(struct node *node) {
+	if(node == NULL)
+		return;
+	release(node->next);
+	free(node);
+}
+
+struct node* pop(struct node *node) {
+	struct node *ret = node->next;
+	free(node);
+	return ret;
+}
+
+struct node* push(struct node *node, struct node *head) {
+	return head->next = node;
+}
+
+void que(struct node *node, struct node *tail) {
+	if(node->next == NULL) {
+		node->next = tail;
+		return;
+	}
+	que(node->next, tail);
+}
+
+int size(struct node *node) {
+	if(node == NULL)
+		return 0;
+	
+	return 1 + size(node->next);
+}
---
+++ src/startmenu.h
@@ -0,0 +1,10 @@
+#ifndef __STARTMENU__
+#define __STARTMENU__
+	
+#include "pebble.h"
+
+extern void create_startmenu();
+extern void send_appmessage(int index);
+extern void remove_startscreen();
+
+#endif
