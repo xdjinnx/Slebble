f7ee8f9b296fd6a17687d51e522f15c957693006
Victor Häggqvist <victor@snilius.com>
2015-01-15 22:52:41 +0100

    And the sort function

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -123,6 +123,43 @@ var Slebble = (function(Pebble, navigator) {
     }
   };
 
+  var _slTimeSort = function(a, b){
+    if (a.time === b.time) // on equal
+      return 0;
+
+    // if any of them is Nu
+    if (a.time === 'Nu')
+      return -1;
+    else if (b.time === 'Nu')
+      return 1;
+
+    // if one is minute
+    else if (a.time.search(':') === -1 && b.time.search(':') > 0)
+      return -1;
+    else if (a.time.search(':') > 0 && b.time.search(':') === -1)
+      return 1;
+
+    else if (a.time.search(':') === -1 && b.time.search(':') === -1) {
+      var at = parseInt(a.time.substr(0, a.time.length-4));
+      var bt = parseInt(b.time.substr(0, b.time.length-4));
+      //console.log(at+'<'+bt+' '+(at<bt?'true -1':'false 1'));
+      return at<bt?-1:1;
+    } else {
+      var ah = parseInt(a.time.split(':')[0]);
+      var bh = parseInt(b.time.split(':')[0]);
+
+      if (ah === bh){
+        var am = parseInt(a.time.split(':')[1]);
+        var bm = parseInt(b.time.split(':')[1]);
+        //console.log(am+'<'+bm+' '+(am<bm?'true -1':'false 1'));
+        return am<bm?-1:1;
+      } else {
+        //console.log(ah+'<'+bh+' '+(ah<bh?'true -1':'false 1'));
+        return ah<bh?-1:1;
+      }
+    }
+  };
+
   /**
    * Filter function for rides to be used with array.filter
    * @param ride        A ride object, se sample in _SLRealtimeCallback
