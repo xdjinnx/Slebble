98f8692953d6c65e9ede21c7e18e8ca9a5bf8003
Peter Lundberg <peter.m.lundberg@gmail.com>
2015-03-17 02:21:49 +0100

    Add webpack building for the js files in the Slebble watch app.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,17 @@ var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     shell = require('gulp-shell'),
     stylish = require('jshint-stylish'),
-    watch = require('gulp-watch'),
-    mocha = require('gulp-mocha');
+    webpack = require('gulp-webpack');
+
+gulp.task('webpack', function() {
+  return gulp.src('./src/js/main.js')
+    .pipe(webpack({
+      output: {
+        filename: 'pebble-js-app.js'
+      }
+    }))
+    .pipe(gulp.dest('./src/js/'));
+});
 
 gulp.task('lint', function(){
   return gulp.src('./src/js/pebble-js-app.js')
@@ -15,14 +24,5 @@ gulp.task('build', shell.task([
   'pebble build'
 ]));
 
-gulp.task('mocha', function() {
-  return gulp.src(['test/*.js'], { read: false })
-    .pipe(mocha({ reporter: 'list' }));
-});
-
-gulp.task('watch-tests', function () {
-  gulp.watch(['test/tests.js', 'src/js/*'], ['mocha']);
-});
-
 // define tasks here
-gulp.task('default', ['lint', 'build']);
+gulp.task('default', ['webpack', 'lint', 'build']);
---
+++ src/js/main.js
@@ -0,0 +1,76 @@
+var Slebble = require("./slebble.js");
+
+Pebble.addEventListener('ready',
+            function(e) {
+              //console.log('ready');
+              var response = localStorage.data;
+              if (response !== undefined && response !== '') {
+                //console.log('has data');
+                response = JSON.parse(localStorage.data);
+                //console.log('saved data'+JSON.stringify(response));
+                Slebble.loadConfig(response);
+                var stations = [];
+                for(var i = 0; i < response.route.length; i++) {
+                  var ad = {};
+                  ad.index = i;
+                  ad.from = response.route[i].from.replace(/\053/g, ' ');
+                  ad.nr = response.route.length;
+                  stations.push(ad);
+                }
+                Slebble.addStation(stations);
+              } else {
+                var ad = {};
+                ad.index = 0;
+                ad.from = 'No configuration';
+                ad.nr = 1;
+                ad = [ad];
+                Slebble.addStation(ad);
+              }
+            });
+
+
+Pebble.addEventListener('showConfiguration',
+            function(e) {
+              if (localStorage.data) {
+                Pebble.openURL('http://diesel-ability-711.appspot.com/webconfig/index.html?version=2.0' + '&setting=' + localStorage.data);
+              }else {
+                Pebble.openURL('http://diesel-ability-711.appspot.com/webconfig/index.html?version=2.0');
+              }
+            });
+
+Pebble.addEventListener('webviewclosed',
+            function(e) {
+              if (e.response === 'reset') {
+                localStorage.removeItem('data');
+              } else if (e.response !== 'CANCELLED' && e.response.substring(0, 12) !== '{"route": []' && e.response !== '{}') {
+                localStorage.setItem('data', e.response);
+
+                if (localStorage.data) {
+                  var response = JSON.parse(localStorage.data);
+                  Slebble.loadConfig(response);
+
+                  var stations = [];
+                  for(var i = 0; i < response.route.length; i++) {
+                    var ad = {};
+                    ad.index = i;
+                    ad.from = response.route[i].from.replace(/\053/g, ' ');
+                    ad.nr = response.route.length;
+                    stations.push(ad);
+                  }
+                  
+                  Slebble.addStation(stations);
+
+                }
+              }
+            });
+
+Pebble.addEventListener('appmessage',
+            function(e) {
+              if(e.payload[1] !== 0) {
+                var response = JSON.parse(localStorage.getItem('data'));
+                Slebble.loadConfig(response);
+                Slebble.requestRides(e.payload[1]-1);
+              } else {
+                Slebble.requestGeoRides();
+              }
+            });
