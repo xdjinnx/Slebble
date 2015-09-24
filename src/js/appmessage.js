ecfc5bc4a46a25432043d3b6eb25e3526ef9432d
Peter Lundberg <peter.m.lundberg@gmail.com>
2015-09-24 03:28:29 +0200

    Add docs and created tests for appmessage.js.

--- src/js/appmessage.js
+++ src/js/appmessage.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/**
+ * Send a set of stations to the pebble watch
+ * @param {Object[]} stations Set of station objects
+ * @param {number} packageKey A unique key that a set of messages should have
+ */
 var _addStation = function(stations, packageKey) {
   if(stations.length < 1)
     return;
@@ -20,6 +25,11 @@ var _addStation = function(stations, packageKey) {
   );
 };
 
+/**
+ * Send a set of rides to the pebble watch
+ * @param {Object[]} depatureList Set of ride objects
+ * @param {number} packageKey A unique key that a set of messages should have
+ */
 var _addRide = function(depatureList, packageKey) {
   if(depatureList.length < 1)
     return;
@@ -42,8 +52,14 @@ var _addRide = function(depatureList, packageKey) {
   );
 };
 
+/**
+ * Send a error message to the pebble watch
+ * @param {string} title The title of the message or bigger text
+ * @param {string} subtitle The subtitle of the message or smaller text
+ * @param {number} packageKey A unique key that a set of messages should have
+ */
 var _appMessageError = function(title, subtitle, packageKey) {
-  console.log('sending error '+title+' '+subtitle);
+  //console.log('sending error '+title+' '+subtitle);
   Pebble.sendAppMessage({
       '0': 3,
       '1': 0,
