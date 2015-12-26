00861797d635ec02de79e442c9217cb0283be821
Peter Lundberg <peter.m.lundberg@gmail.com>
2015-12-26 02:16:47 +0100

    Add cache directories to travis and add a before_install script.

---
+++ before_install.sh
@@ -0,0 +1,11 @@
+if [ ! -d "pebble-dev/pebble-sdk-4.0-linux64" ]; then
+  echo "No pebble-dev/pebble-sdk-4.0-linux64 found"
+  wget https://s3.amazonaws.com/assets.getpebble.com/pebble-tool/pebble-sdk-4.0-linux64.tar.bz2 -O PebbleSDK.tar.bz2
+  mkdir pebble-dev
+  tar -xvjf PebbleSDK.tar.bz2 -C pebble-dev
+fi
+
+wget https://cmocka.org/files/1.0/cmocka-1.0.1.tar.xz
+tar -xvf cmocka-1.0.1.tar.xz
+mkdir cmocka-1.0.1/build
+cmake -DCMAKE_INSTALL_PREFIX=/home/travis/build/xdjinnx/Slebble/cmocka -DCMAKE_BUILD_TYPE=Debug -Bcmocka-1.0.1/build -Hcmocka-1.0.1
