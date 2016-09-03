108324d3c37ff890e1e0947f4d6bec5711ec153c
Peter Lundberg <peter.lundberg@digiexam.se>
2016-09-03 21:07:36 +0200

    Removed unneeded dependencies.

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,45 @@
 /* eslint strict: 0 */
 var gulp = require('gulp'),
-    shell = require('gulp-shell'),
-    webpack = require('webpack-stream'),
-    stripCode = require('gulp-strip-code'),
-    eslint = require('gulp-eslint'),
-    concat = require('gulp-concat');
+	shell = require('gulp-shell'),
+	webpack = require('webpack-stream'),
+	stripCode = require('gulp-strip-code'),
+	eslint = require('gulp-eslint'),
+	concat = require('gulp-concat');
 
 gulp.task('webpack', ['lint'], function() {
-  return gulp.src('./src/js/main.js')
-    .pipe(webpack({
-      output: {
-        filename: 'pebble-js-app.js'
-      },
-      module: {
-        loaders: [{
-          loader: 'babel-loader'
-        }]
-      }
-    }))
-    .pipe(gulp.dest('./src/js/build/'));
+	return gulp.src('./src/js/main.js')
+		.pipe(webpack({
+			output: {
+				filename: 'pebble-js-app.js'
+			}
+		}))
+		.pipe(gulp.dest('./src/js/build/'));
 });
 
 gulp.task('lint', function(){
-  return gulp.src(['./src/js/*.js', '!./src/js/build/*'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+	return gulp.src(['./src/js/*.js', '!./src/js/build/*'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
 });
 
 gulp.task('strip', ['webpack'],function(){
-  return gulp.src(['./src/js/build/pebble-js-app.js'])
-    .pipe(stripCode({
-      start_comment: "test-block",
-      end_comment: "end-test-block"
-    }))
-    .pipe(gulp.dest('./src/js/build/'));
+	return gulp.src(['./src/js/build/pebble-js-app.js'])
+		.pipe(stripCode({
+			start_comment: "test-block",
+			end_comment: "end-test-block"
+		}))
+		.pipe(gulp.dest('./src/js/build/'));
 });
 
 gulp.task('concat', ['strip'], function() {
-  return gulp.src(['./src/js/build/pebble-js-app.js', './node_modules/trackjs/tracker.js'])
-    .pipe(concat('pebble-js-app.js'))
-    .pipe(gulp.dest('./src/js/build/'));
+	return gulp.src(['./src/js/build/pebble-js-app.js', './node_modules/trackjs/tracker.js'])
+		.pipe(concat('pebble-js-app.js'))
+		.pipe(gulp.dest('./src/js/build/'));
 });
 
 gulp.task('build', ['concat'], shell.task([
-  'pebble build'
+	'pebble build'
 ]));
 
 // define tasks here
